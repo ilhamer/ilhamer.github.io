684a5aa686b07573a4e27571bbb6032541cea1bf
ilhamer <ilhamer555@gmail.com>
2022-12-27 10:36:19 +0700

    first commit

---
+++ jsMe/javaScriptMe.js
@@ -0,0 +1,201 @@
+// let navbar =` <!-- navbar start -->
+// <nav class="navbarMe navbar navbar-expand-lg navbar-dark shadow-sm fixed-top">
+//   <div class="container-fluid ms-auto">
+//     <a class="navbar-brand" href="#">Ilham Ridho</a>
+//     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+//       <span class="navbar-toggler-icon"></span>
+//     </button>
+//     <div class="collapse navbar-collapse" id="navbarNav">
+//       <ul class="navbar-nav">
+//         <li class="nav-item">
+//           <a class="nav-link active" aria-current="page" href="#home">Home</a>
+//         </li>
+//         <li class="nav-item">
+//           <a class="nav-link" href="#about">About</a>
+//         </li>
+//         <li class="nav-item">
+//           <a class="nav-link" href="#project">Project</a>
+//         </li>
+//         <li class="nav-item">
+//           <a class="nav-link" href="#contact">Contact Me</a>
+//         </li>
+    
+       
+//       </ul>
+//     </div>
+//   </div>
+// </nav>
+
+// <!-- navbar end -->
+
+
+
+// <!-- jumboTron -->
+// <section class="jumbotron text-center" >
+// <img src="../imgMe/ilham.jpg" alt="Ilham Ridho" width="200" class="rounded-circle img-thumbnail">
+// <h1 class="display-4">Ilham Ridho</h1>
+// <p class="lead">Saya adalah seorang web Developer Junior,yang ingin terus berkembang skill dan kemampuan dalam pengembangan sebuah web & aplikasi mobile</p>
+
+
+// <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="10" d="M0,128L40,128C80,128,160,128,240,149.3C320,171,400,213,480,213.3C560,213,640,171,720,154.7C800,139,880,149,960,149.3C1040,149,1120,139,1200,117.3C1280,96,1360,64,1400,48L1440,32L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
+// </section>
+// <!-- jumboTron End -->
+
+
+// <!-- about -->
+// <section id="about">
+// <div class="container">
+// <div class="row text-center mb-4">
+//   <div class="col">
+//     <h2>About Me</h2>
+//   </div>
+// </div>
+// <div class="row  justify-content-center fs-5 text-center">
+//   <div class="col-4">
+//     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus inventore odit illum sapiente amet ullam aut et esse ex beatae! Eos quidem illo necessitatibus quo commodi a animi? Qui, consequatur.</p>
+//   </div>
+//   <div class="col-4">
+//     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea maiores nisi fugiat sunt accusamus accusantium corporis. Odio nostrum dicta, consequatur voluptatum nam ut.</p>
+//   </div>
+// </div>
+// </div>
+// <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#4f6e6acc" fill-opacity="10" d="M0,224L40,229.3C80,235,160,245,240,229.3C320,213,400,171,480,128C560,85,640,43,720,37.3C800,32,880,64,960,85.3C1040,107,1120,117,1200,133.3C1280,149,1360,171,1400,181.3L1440,192L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
+// </section>
+
+// <!-- end about -->
+
+
+// <!-- project -->
+// <section id="project">
+// <div class="container">
+// <div class="row text-center mb-3">
+//   <div class="col">
+//     <h2>My Project</h2>
+//   </div>
+// </div>
+
+// <div class="row justify-content-evently">
+// <div class="col-md-4 mb-3">
+//   <div class="card"">
+//     <img src="../imgMe/img1.jpg" class="card-img-top img-thumbnail" alt="project 1">
+//     <div class="card-body">
+//       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//     </div>
+//   </div>
+// </div>
+// <div class="col-md-4 mb-3">
+//   <div class="card"">
+//     <img src="../imgMe/img2.jpg" class="card-img-top img-thumbnail" alt="project 3">
+//     <div class="card-body">
+//       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//     </div>
+//   </div>
+// </div>
+// <div class="col-md-4 mb-3">
+//   <div class="card"">
+//     <img src="../imgMe/img3.jpg" class="card-img-top img-thumbnail" alt="project 4">
+//     <div class="card-body">
+//       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//     </div>   
+//   </div>
+// </div>
+// <div class="col-md-4 mb-3">
+//   <div class="card"">
+//     <img src="../imgMe/img4.jpg" class="card-img-top img-thumbnail" alt="project 4">
+//     <div class="card-body">
+//       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//     </div>   
+//   </div>
+// </div>
+// <div class="col-md-4 mb-3">
+//   <div class="card"">
+//     <img src="../imgMe/img5.jpg" class="card-img-top img-thumbnail" alt="project 4">
+//     <div class="card-body">
+//       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//     </div>   
+//   </div>
+// </div>
+
+
+// </div>
+// </div>
+// <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="10" d="M0,64L40,53.3C80,43,160,21,240,42.7C320,64,400,128,480,138.7C560,149,640,107,720,80C800,53,880,43,960,53.3C1040,64,1120,96,1200,90.7C1280,85,1360,43,1400,21.3L1440,0L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
+// </section>
+
+// <!-- akhir project -->
+
+// <!-- contact me -->
+// <section id="contact">
+// <div class="container">
+// <div class="row text-center mb-3">
+//   <div class="col">
+//     <h2>Contact Me</h2>
+//   </div>
+// </div>
+// <div class="row justify-content-center mb-5">
+//   <div class="col-md-6">
+//     <form>
+//       <div class="mb-3">
+//         <label for="name" class="form-label">Nama</label>
+//         <input type="text" class="form-control" id="name" aria-describedby="name">
+       
+//       </div>
+//       <div class="mb-3">
+//         <label for="email" class="form-label">Email</label>
+//         <input type="email" class="form-control" id="email">
+//       </div>
+//       <div class="mb-3">
+//         <label for="pesan" class="form-label">Tinggalkan Kritik & saran di sini</label>
+//         <textarea class="form-control" id="pesan" rows="3"></textarea>
+//       </div>
+//       <button type="submit" class="btn btn-primary">Kirim</button>
+//       </form>
+//   </div>
+// </div>
+// </div>
+// <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill=" #4f6e6a" fill-opacity="10" d="M0,256L40,261.3C80,267,160,277,240,266.7C320,256,400,224,480,197.3C560,171,640,149,720,165.3C800,181,880,235,960,256C1040,277,1120,267,1200,224C1280,181,1360,107,1400,69.3L1440,32L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
+// </section>
+
+// <!-- contact me end -->
+
+// <!-- Footer -->
+// <footer class="footer text-white text-center pb-3">
+// <p>Created by Ilham Ridho <a href="https//www.instagram.com/ridodow/" class="text-white fw-bold">Ilham Ridho</a></p>
+// </footer>
+// <!-- Footer end --> `;
+
+// $('body').html(navbar)
+
+
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbyV0IvMEnq3KGL30m6Vrv3k0ReDMg6QYK8KTjJLg7hHDDnBQvkR-KOiZQehamNOD1AN/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const send = document.querySelector('.btn-send');
+  const load = document.querySelector('.btn-load');
+  const alert = document.querySelector('.alert');
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    load.classList.toggle('d-none');
+    send.classList.toggle('d-none');
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        alert.classList.toggle('d-none');
+          load.classList.toggle('d-none');
+          send.classList.toggle('d-none');
+          form.reset()
+        console.log('Success!', response)})
+      .catch(error => console.error('Error!', error.message))
+  })
+ 
+
+
+
+  
+  
+
+
+
+
+
+
